4faf554405821dc27f5ec252ba3bc82ded868825
sajjad <radsjad9@gmail.com>
2022-11-16 07:25:34 +0330

    create project with bootstrap & scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { SloginComponent } from './slogin/slogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'List', component: ListComponent },
+{ path: 'form', component: FormComponent },
+{path : 'sLogin' , component : SloginComponent},
+{ path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { SloginComponent } from './slogin/slogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FormComponent,
+    ListComponent,
+    SloginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
